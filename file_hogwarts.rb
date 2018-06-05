3c08336503d94edf15ec2f8cc2df0f7fa4a83304
RyleeBerger <ryleehaddon24@outlook.com>
2018-06-05 17:56:14 +0000

    automatically backed up by learn

---
+++ file_hogwarts.rb
@@ -0,0 +1,27 @@
+puts "Hi, you've been invited to a party! What is your name?"
+
+guest_name = gets.chomp.capitalize
+
+puts "What is the name of the party?"
+
+party_name = gets.chomp.capitalize
+
+puts "What is the date of the party?"
+
+date = gets.chomp.capitalize
+
+puts "What time is the party?"
+
+time = gets.chomp
+
+puts "What is the name of the host?"
+
+host_name = gets.chomp
+
+puts "Dear #{guest_name},
+
+You are cordially invited to #{party_name} on #{date} at #{time}. 
+
+Sincerely,
+
+#{host_name}"
---
+++ methods_2.rb
@@ -0,0 +1,24 @@
+#def name_1
+  #name_one = "Charlie"
+ # puts "Hello #{name_one}!"
+#end
+
+#def name_2
+  #name_two = "Randi"
+  #puts "Hello #{name_two}!"
+#end
+
+#def name_3
+  #name_three = "Gabriella"
+  #puts "Hello #{name_three}!"
+#end
+
+#name_1
+#name_2
+#name_3
+
+def say_hello(your_name, my_name = "King Kong")
+  puts "Hello #{your_name}! I'm #{my_name}."
+end
+
+say_hello("Hibba")
